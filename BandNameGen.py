4dd57f25d32fb29356d615e308973ea4d4b63c37
koala-is-coding <123737693+koala-is-coding@users.noreply.github.com>
2024-02-17 09:00:18 +0600

    Day1project

---
+++ BandNameGen.py
@@ -0,0 +1,7 @@
+#Band Name Generator
+n=input('What is your name?');
+print('Hello'+n);
+pet = input('What is the name of your pet?');
+city=input('What is the city you grew up in?');
+
+print('Your suggested band name is::'+pet+' '+city);
